9242197223ce03a364bb8815172ee2c489662f4d
ArunKishoreVoleti <arunkishorevoleti@gmail.com>
2025-02-06 14:37:22 +0530

    Update pyutils.py
    Added few more functions

--- arunpyutils/pyutils.py
+++ arunpyutils/pyutils.py
@@ -1,21 +1,22 @@
 class Pyutils:
 
-    #A Method to find if a number is even or not
+    # A method to find if a number is even or not
     def is_even(self, number):
         if number%2==0 and number>0:
             return True
         return False
 
-    #A Method to find if a number is odd or not
+    # A method to find if a number is odd or not
     def is_odd(self, number):
         if number%2==1 and number>0:
             return True
         return False
 
-    #A Method to find Sum of First N Numbers
+    # A method to find Sum of First N Numbers
     def sum_n(self, number):
         return (number * (number+1))//2
 
+    # A method to find if a number is prime or not
     def is_prime(self, number):
         if number<2:
             return False
@@ -23,4 +24,15 @@ class Pyutils:
             if number%fact==0:
                 return False
         return True
-        
+    
+    # A method to find if an year is leap or not
+    def is_leap_year(self, year):
+        if year%400 == 0 or (year%4 == 0 and year%100 != 0):
+            return True
+        return False
+    
+    # A method to identify if a number is palendrome or not
+    def is_palendrome_number(self, number):
+        if str(number) == str(number)[::-1]:
+            return True
+        return False        
